50311712130b14e243f52b4a98ce1e4386f32685
ShahzaibKhaan <shahzenkhaan@gmail.com>
2025-11-26 23:52:07 +0500

    Add existing folder and my changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@ const todo = document.querySelector('#pending');
 const progress = document.querySelector('#progress');
 const complete = document.querySelector('#complete');
 
-let dragElement =null;
+let dragElement = null;
 console.log(
-    complete, todo,progress
+    complete, todo, progress
 )
 const task = document.querySelectorAll('.task')
 task.forEach(task => {
     task.addEventListener("drag", (e) => {
         // console("dragging", e)
-        dragElement= task;
+        dragElement = task;
 
     })
 })
-function addDragEventOnColumn(column){
+function addDragEventOnColumn(column) {
     column.addEventListener("dragenter", (e) => {
         e.preventDefault();
         column.classList.add("hover-over");
@@ -23,13 +23,13 @@ function addDragEventOnColumn(column){
         e.preventDefault();
         column.classList.remove("hover-over");
     })
-    column.addEventListener("dragover", (e) =>{
+    column.addEventListener("dragover", (e) => {
         e.preventDefault();
     })
-    column.addEventListener("drop", (e) =>{
+    column.addEventListener("drop", (e) => {
         e.preventDefault();
-        console.log("Dropped",dragElement, column );
-        
+        console.log("Dropped", dragElement, column);
+
         column.appendChild(dragElement)
         column.classList.remove("hover-over");
     })
@@ -37,5 +37,4 @@ function addDragEventOnColumn(column){
 addDragEventOnColumn(todo);
 addDragEventOnColumn(progress);
 addDragEventOnColumn(complete);
-
-
+ 
